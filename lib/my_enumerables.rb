94f3e6d1f856c12e5275b9d765e843c42a50fb3b
Katsudon27 <katsudon1127@gmail.com>
2026-05-15 12:08:40 +1000

    Add definition for my_all

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -20,6 +20,16 @@ module Enumerable
 
     match
   end
+
+  def my_all?
+    return to_enum(:my_all?) unless block_given?
+
+    self.my_each do |element|
+      return false if !yield(element)
+    end
+
+    true
+  end
 end
 
 # You will first have to define my_each
