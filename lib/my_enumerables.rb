ceff3b27238c2c55c1dce3ed55ffafc08b34fdaf
Katsudon27 <katsudon1127@gmail.com>
2026-05-15 11:55:53 +1000

    Add definition for my_each

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -8,4 +8,13 @@ end
 # to this method
 class Array
   # Define my_each here
+  def my_each
+    return to_enum(:my_each) unless block_given?
+
+    self.length.times do |index|
+      yield(self[index])
+    end
+
+    self
+  end
 end
